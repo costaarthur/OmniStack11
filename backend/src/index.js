79b4561d7e5bfafa05273f252fb944374dc93cae
costaarthur <costaarthur@hotmail.com>
2020-03-25 16:04:20 -0300

    gg controllers cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const corts = require('cors')
 const routes = require('./routes')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
+app.listen(3333);
+
+
+
 /**
  * Métodos HTTP:
- * 
+ *
  * GET: buscar uma informação do backend
  * POST: criar uma informação no backend
  * PUT: alterar uma informação no backend
@@ -18,7 +24,7 @@ app.use(routes);
 
 /**
  * Tipos de parâmetros:
- * 
+ *
  * Query Params: Parâmetros enviados na rota após "?" (Filtros, paginação) (?nome=x)
  * Route Params: Usados para identificar recursos (/users/1) (:id)
  * Request Body: Corpo da requisição, cria ou altera recursos
@@ -31,11 +37,10 @@ app.use(routes);
 
 /**
  * Formas de usar as DBs
- * 
+ *
  * Driver: SELECT * FROM users
  * Query Builder: table('users')
  */
 
 
 
-app.listen(3333);
