fef2e9a1d3de47e963eb0c3d41e4ebd39c620daa
costaarthur <costaarthur@hotmail.com>
2020-03-26 12:36:30 -0300

    all api connections added gg front

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -14,17 +14,29 @@ export default function Register() {
   const [city, setCity] = useState('')
   const [uf, setUf] = useState('')
 
+  const history = useHistory();
 
-  function handleRegister(e) {
+
+  async function handleRegister(e) {
     e.preventDefault();
 
-    console.log({
+    const data = {
       name,
       email,
       whatsapp,
       city,
       uf
-    })
+    }
+
+    try {
+      const response = await api.post('ongs', data)
+
+      alert(`Seu ID de acesso: ${response.data.id}`)
+
+      history.push('/')
+    } catch (err) {
+      alert('Erro no cadastro, tente novamente.')
+    }
   }
 
   return (
