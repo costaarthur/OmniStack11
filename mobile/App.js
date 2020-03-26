2427363919e6bb0b3114909ce57d05abff802e7b
costaarthur <costaarthur@hotmail.com>
2020-03-26 17:51:57 -0300

    routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hail Clã</Text>
+    <View >
+      <Text>Hail Clã</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-
-  title: {
-    fontSize: 30,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-});
